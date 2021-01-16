2c494c5a9941274a3e945c2b84568a55b20ef232
gokalpfirat05 <gokalp.firat@onedio.com>
2021-01-16 20:45:23 +0300

    Service worker

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 import { searchRobots, requestRobots } from './reducers';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 // ReactDOM.render(
 //   <React.StrictMode>
@@ -25,6 +26,11 @@ ReactDOM.render(
     <App />
   </Provider>
 , document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
